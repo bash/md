b85afee2d3c4533bb0a13df80981c0aae059e5a4
Tau Gärtli <git@tau.garden>
2024-06-01 14:46:03 +0200

    Test lists

--- tests/snapshot.rs
+++ tests/snapshot.rs
@@ -1,6 +1,7 @@
 use insta::{assert_snapshot, glob};
 use matte::{render, supported_parser_options, Options};
-use pulldown_cmark::Parser;
+use pulldown_cmark::{Event, MetadataBlockKind, Options as ParserOptions, Parser, Tag};
+use serde::Deserialize;
 use std::fs::read_to_string;
 
 #[test]
@@ -15,7 +16,36 @@ fn test_snippets() {
 fn render_to_string(input: &str) -> String {
     let parser = Parser::new_ext(input, supported_parser_options());
     let mut buffer = Vec::new();
-    let options = Options::plain_text(120);
+    let snippet_options = read_snippet_options(input);
+    let options = Options::plain_text(snippet_options.width.unwrap_or(120));
     render(parser, &mut buffer, options).unwrap();
     String::from_utf8(buffer).unwrap()
+}
+
+fn read_snippet_options(input: &str) -> SnippetOptions {
+    toml::de::from_str(&extract_frontmatter(input)).unwrap()
+}
+
+fn extract_frontmatter(input: &str) -> String {
+    let parser = Parser::new_ext(
+        input,
+        ParserOptions::ENABLE_PLUSES_DELIMITED_METADATA_BLOCKS,
+    );
+    let mut frontmatter = String::new();
+    let mut inside_frontmatter = false;
+    for event in parser {
+        match event {
+            Event::Start(Tag::MetadataBlock(MetadataBlockKind::PlusesStyle)) => {
+                inside_frontmatter = true
+            }
+            Event::Text(text) if inside_frontmatter => frontmatter.push_str(&text),
+            _ => break,
+        }
+    }
+    frontmatter
+}
+
+#[derive(Deserialize)]
+struct SnippetOptions {
+    width: Option<u16>,
 }
---
+++ tests/snippets/lists.md
@@ -0,0 +1,63 @@
++++
+width = 40
++++
+
+# Lists
+
+## Block Quote Followed by List
+> Block quote
+- Foo
+- Bar
+- Baz
+
+## Ordered List
+1. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+   Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+2. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+   1. Nested
+   2. Things
+3. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+## Unordered List
+* Foo
+* Bar
+* Baz
+
+## List Formatting
+1. **Bold Text**
+2. Code Block
+   ```js
+   console.log('hello world')
+   ```
+   with text after code block
+3. Sublist with text before
+   * foo
+   * bar
+   * baz
+   and text after
+
+## Quoted List
+> 1. Wow
+> 2. What
+> 3. A
+> 4. Quote
+
+## Deeply Nested List
+1. Wow
+   1. Much
+      1. Nesting
+         1. Such
+            1. Deep
+               1. List
+* Wow
+   * Much
+      * Nesting
+         * Such
+            * Deep
+               * List
+
+
+## Tasks
+* [ ] buy groceries
+* [x] clean kitchen
+* [x] fix bathroom lights
